877f4ac1a986673fab646cfa194099bed7903a9b
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-11-26 16:55:13 +0900

    no message

--- src/main/java/com/example/hpseahouse/seahouseController.java
+++ src/main/java/com/example/hpseahouse/seahouseController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class seahouseController {
+
     @GetMapping("/seahouse")
     public String seahouse(Model model){
         model.addAttribute("message","seahouse iouzima");
